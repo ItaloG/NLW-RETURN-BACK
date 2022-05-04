5bb3dc3dfa3bb2fe757d57678666b9694582c288
Italo Gabriel <italog03@gmail.com>
2022-05-04 16:01:00 -0300

    feedback route

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "073534faec98f6",
+    pass: "d00400b5bb1fc4",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Italo Gabriel <italog03@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
